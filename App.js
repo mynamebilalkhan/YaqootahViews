87ea06345da563ac136183f69a78ff0daa2cd1ee
BilalK <test.dev427@gmail.com>
2025-03-07 15:21:07 +0500

    Minor change in App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
-import {store} from './redux/store';
+import {store} from './src/redux/store';
 
 export default function App() {
   return (
